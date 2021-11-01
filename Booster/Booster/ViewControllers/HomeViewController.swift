8a904425bc8331b48ab81854da2578cc9e961565
Hiju <rose6649@likelion.org>
2021-11-01 12:16:27 +0900

    add: 홈 화면 UI 구성

--- Booster/Booster/ViewControllers/HomeViewController.swift
+++ Booster/Booster/ViewControllers/HomeViewController.swift
@@ -1,8 +1,28 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    
+
+    // MARK: Properties
+
+    @IBOutlet weak var kcalLabel: UILabel!
+    @IBOutlet weak var timeActiveLabel: UILabel!
+    @IBOutlet weak var kmLabel: UILabel!
+    @IBOutlet weak var todayTotalStepCountLabel: UILabel!
+    @IBOutlet weak var goalLabel: UILabel!
+
+    // MARK: Life Cycles
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
     }
+
+}
+
+extension HomeViewController {
+
+    private func configure() {
+        
+    }
+
 }
