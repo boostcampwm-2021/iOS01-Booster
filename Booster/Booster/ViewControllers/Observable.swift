d4bd263fd65a524cc578e8db7cbd7c54057ad436
Hani <nellingprocess@sju.ac.kr>
2021-11-01 18:35:42 +0900

    add: bind + observable을 위한 클래스

---
+++ Booster/Booster/ViewControllers/Observable.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+final class Observable<T> {
+
+  private var listener: ((T) -> Void)?
+
+  var value: T {
+    didSet {
+      self.listener?(self.value)
+    }
+  }
+
+  init(_ value: T) {
+    self.value = value
+  }
+
+  func bind(_ closure: @escaping (T) -> Void) {
+    closure(value)
+    listener = closure
+  }
+
+}
