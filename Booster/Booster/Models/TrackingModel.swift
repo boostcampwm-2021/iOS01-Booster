507815055688511554258ca332ec8c318f7b8620
KTH-INHA-16 <kim1120318@naver.com>
2021-11-03 20:32:30 +0900

    add:  tracking models

--- Booster/Booster/Models/TrackingModel.swift
+++ Booster/Booster/Models/TrackingModel.swift
@@ -1,14 +1,36 @@
 import Foundation
 
 struct TrackingModel {
-    let startDate: Date
+    var startDate: Date
     var endDate: Date
     var steps: Int
     var calories: Int
     var seconds: Int
     var distance: Double
-    var coordinates: [Coordinate?]
+    var coordinates: [Coordinate]
     var milestones: [MileStone]
     var title: String
     var content: String
+
+    init(startDate: Date = Date(),
+         endDate: Date = Date(),
+         steps: Int = 0,
+         calories: Int = 0,
+         seconds: Int = 0,
+         distance: Double = 0,
+         coordinates: [Coordinate] = [],
+         milestones: [MileStone] = [],
+         title: String = "",
+         content: String = "") {
+        self.startDate = startDate
+        self.endDate = endDate
+        self.steps = steps
+        self.calories = calories
+        self.seconds = seconds
+        self.distance = distance
+        self.coordinates = coordinates
+        self.milestones = milestones
+        self.title = title
+        self.content = content
+    }
 }
--- Booster/Booster/Models/User.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct User {
-    let age: Int
-    private(set) var nickname: String
-    private(set) var gender: String
-    private(set) var height: Int
-    private(set) var weight: Int
-}
