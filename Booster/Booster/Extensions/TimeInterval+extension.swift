649f71b07f35f24a05de95a3547fe74cb6e49353
Hiju <rose6649@likelion.org>
2021-11-03 10:30:09 +0900

    add: timeInterval 익스텐션 추가

--- Booster/Booster/Extensions/TimeInterval+extension.swift
+++ Booster/Booster/Extensions/TimeInterval+extension.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension TimeInterval {
+    private var seconds: Int {
+        return Int(self) % 60
+    }
+
+    private var minutes: Int {
+        return (Int(self) / 60) % 60
+    }
+
+    private var hours: Int {
+        return Int(self) / 3600
+    }
+
+    func stringToMinutesAndSeconds() -> String {
+        return "\(hours)h \(minutes)m"
+    }
+}
