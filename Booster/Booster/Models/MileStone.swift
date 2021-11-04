6b1e7ea98d726c7bd5e55c5298b7afcbdd125da4
KTH-INHA-16 <kim1120318@naver.com>
2021-11-04 15:25:14 +0900

    add:  모델 아카이빙

--- Booster/Booster/Models/MileStone.swift
+++ Booster/Booster/Models/MileStone.swift
@@ -1,11 +1,26 @@
 import Foundation
 
-public final class MileStone: NSObject {
-    let coordinate: Coordinate
-    let imageData: Data
+public final class MileStone: NSObject, NSCoding {
+    var coordinate: Coordinate
+    var imageData: Data
 
     init(latitude: Double = 0, longitude: Double = 0, imageData: Data = Data()) {
         self.coordinate = Coordinate(latitude: latitude, longitude: longitude)
         self.imageData = imageData
+    }
+
+    public func encode(with coder: NSCoder) {
+        coder.encode(coordinate, forKey: "coordinate")
+        coder.encode(imageData, forKey: "imageData")
+    }
+
+    public init?(coder: NSCoder) {
+        coordinate = Coordinate(latitude: 0, longitude: 0)
+        imageData = Data()
+        if let coord = coder.decodeObject(forKey: "coordinate") as? Coordinate,
+              let data = coder.decodeObject(forKey: "imageData") as? Data {
+            coordinate = coord
+            imageData = data
+        }
     }
 }
