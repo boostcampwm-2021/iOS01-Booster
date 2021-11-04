d811e8985909b224993a73790e34f17c535847ef
Hani <nellingprocess@sju.ac.kr>
2021-11-04 15:34:19 +0900

    feat: Observable for bind

--- Booster/Booster/ViewControllers/Observable.swift
+++ Booster/Booster/ViewControllers/Observable.swift
@@ -2,21 +2,21 @@ import Foundation
 
 final class Observable<T> {
 
-  private var listener: ((T) -> Void)?
+    private var listener: ((T) -> Void)?
 
-  var value: T {
-    didSet {
-      self.listener?(self.value)
+    var value: T {
+        didSet {
+            self.listener?(self.value)
+        }
     }
-  }
 
-  init(_ value: T) {
-    self.value = value
-  }
+    init(_ value: T) {
+        self.value = value
+    }
 
-  func bind(_ closure: @escaping (T) -> Void) {
-    closure(value)
-    listener = closure
-  }
+    func bind(_ closure: @escaping (T) -> Void) {
+        closure(value)
+        listener = closure
+    }
 
 }
