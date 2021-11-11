f8988244f7fb401d514dbfab6fd0b7f041472e4b
KTH-INHA-16 <kim1120318@naver.com>
2021-11-11 11:04:03 +0900

    add: feed cell, usecase, configurator 추가

---
+++ Booster/Booster/Views/Configurator/CellConfigurator.swift
@@ -0,0 +1,8 @@
+//
+//  CellConfigurator.swift
+//  Booster
+//
+//  Created by 김태훈 on 2021/11/10.
+//
+
+import Foundation
---
+++ Booster/Booster/Views/Feed/FeedCell.swift
@@ -0,0 +1,12 @@
+//
+//  FeedCell.swift
+//  Booster
+//
+//  Created by 김태훈 on 2021/11/10.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+}
