031617bf0edc58c7f7dbbe7a630024fbb1604410
KTH-INHA-16 <kim1120318@naver.com>
2021-11-11 11:10:22 +0900

    add: close #63 feed 리스트 보여주기 기능

--- Booster/Booster/Usecases/FeedUseCase.swift
+++ Booster/Booster/Usecases/FeedUseCase.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+class FeedUseCase {
+    enum EntityName {
+        static let tracking = "Tracking"
+    }
+
+    private let repository: RepositoryManager
+    private let entity: String
+
+    init() {
+        entity = "Tracking"
+        repository = RepositoryManager()
+    }
+
+    func fetch(completion handler: @escaping ([TrackingRecord]) -> Void) {
+        repository.fetch(type: EntityName.tracking) { (response: Result<[Tracking], Error>) in
+            switch response {
+            case .success(let result):
+                var trackingModels: [TrackingRecord] = []
+                result.forEach { value in
+                    if let startDate = value.startDate,
+                       let title = value.title,
+                       let imageData = value.imageData {
+                        let trackingRecord = TrackingRecord(title: title,
+                                                            date: startDate,
+                                                            distance: value.distance,
+                                                            totalSteps: Int(value.steps),
+                                                            imageData: imageData)
+                        trackingModels.append(trackingRecord)
+                    }
+
+                    handler(trackingModels)
+                }
+
+            case .failure:
+                handler([])
+            }
+        }
+    }
+}
--- Booster/Booster/Views/Configurator/CellConfigurator.swift
+++ Booster/Booster/Views/Configurator/CellConfigurator.swift
@@ -5,4 +5,44 @@
 //  Created by 김태훈 on 2021/11/10.
 //
 
-import Foundation
+import UIKit
+
+protocol ConfigurableCell {
+    associatedtype DataType
+    func configure(data: DataType)
+}
+
+protocol CellConfigurator {
+    static var reuseId: String { get }
+    func configure(cell: UIView)
+}
+
+class CollectionCellConfigurator<CellType: ConfigurableCell, DataType>: CellConfigurator where CellType.DataType == DataType, CellType: UICollectionViewCell {
+    static var reuseId: String { return String(describing: CellType.self) }
+
+    let item: DataType
+
+    init(item: DataType) {
+        self.item = item
+    }
+
+    func configure(cell: UIView) {
+        guard let view = cell as? CellType else { return }
+        view.configure(data: item)
+    }
+}
+
+class ReuseableViewConfigurator<ViewType: ConfigurableCell, DataType>: CellConfigurator where ViewType.DataType == DataType, ViewType: UICollectionReusableView {
+    static var reuseId: String { return String(describing: ViewType.self) }
+
+    let item: DataType
+
+    init(item: DataType) {
+        self.item = item
+    }
+
+    func configure(cell: UIView) {
+        guard let view = cell as? ViewType else { return }
+        view.configure(data: item)
+    }
+}
