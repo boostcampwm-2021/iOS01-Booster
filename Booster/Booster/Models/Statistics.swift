44e13aae692297297b087286a987d479a2295834
Hani <nellingprocess@sju.ac.kr>
2021-11-11 15:39:19 +0900

    add: 통계 날짜 / 제스쳐 / UI 업데이트

--- Booster/Booster/Models/Statistics.swift
+++ Booster/Booster/Models/Statistics.swift
@@ -1,8 +1,7 @@
 import Foundation
 
-struct StatisticsCollection {
-
-    private var statisticsCollection: [Statistics] = []
+struct StatisticsCollection: Equatable {
+    private var statisticsCollection = [Statistics]()
     var count: Int { statisticsCollection.count }
 
     subscript (index: Int) -> Statistics {
@@ -16,22 +15,68 @@ struct StatisticsCollection {
     func maxStatistics() -> Statistics? {
         guard var maxStatistics = statisticsCollection.first else { return nil }
         for statistics in statisticsCollection {
-            if maxStatistics.step < statistics.step { maxStatistics = statistics }
+            maxStatistics = max(maxStatistics, statistics)
         }
+
         return maxStatistics
     }
 
     func averageStatistics() -> Int {
-        guard self.count > 0 else { return 0 }
-        return statisticsCollection.reduce(0) { $0 + $1.step } / self.count
+        guard let startDate = statisticsCollection.first?.date,
+              let endDate = statisticsCollection.last?.date
+        else { return 0 }
+
+        let startDateComponents = Calendar.current.dateComponents([.year, .month, .day], from: startDate)
+        let endDateComponents = Calendar.current.dateComponents([.year, .month, .day], from: endDate)
+
+        let days = Calendar.current.dateComponents([.day], from: startDateComponents, to: endDateComponents).day ?? 1
+
+        let sumStep = statisticsCollection.reduce(0) { $0 + $1.step }
+
+        return sumStep / days
+    }
+    func termOfStatistics(component: Calendar.Component) -> String {
+        guard let startDate = statisticsCollection.first?.date,
+              let endDate = statisticsCollection.last?.date,
+              let lastDate = Calendar.current.date(byAdding: component, value: 1, to: endDate)
+        else { return String() }
+
+        let startDateComponents = Calendar.current.dateComponents([.year, .month, .day], from: startDate)
+        let endDateComponents = Calendar.current.dateComponents([.year, .month, .day], from: lastDate)
+
+        guard let startYear = startDateComponents.year,
+              let startMonth = startDateComponents.month,
+              let startDay = startDateComponents.day,
+              let endYear = endDateComponents.year,
+              let endMonth = endDateComponents.month,
+              let endDay = endDateComponents.day
+        else { return String() }
+
+        let startString = "\(startYear)년 \(startMonth)월 \(startDay)일"
+        var endString = ""
+
+        if startDateComponents.year != endDateComponents.year {
+            endString = "\(endYear)년 \(endMonth)월 \(endDay)일"
+        } else if startDateComponents.month != endDateComponents.month {
+            endString = "\(endMonth)월 \(endDay)일"
+        } else {
+            endString = "\(endDay)일"
+        }
+
+        return "\(startString) - \(endString)"
     }
 
     mutating func append(statistics: Statistics) {
-        self.statisticsCollection.append(statistics)
+        statisticsCollection.append(statistics)
     }
+
 }
 
-struct Statistics {
+struct Statistics: Comparable {
     let date: Date
     let step: Int
+
+    static func < (lhs: Statistics, rhs: Statistics) -> Bool {
+        lhs.step < rhs.step
+    }
 }
--- Booster/Booster/ViewModels/Statistic/StatisticsViewModel.swift
+++ Booster/Booster/ViewModels/Statistic/StatisticsViewModel.swift
@@ -2,25 +2,28 @@ import Foundation
 import HealthKit
 
 final class StatisticsViewModel {
+    // MARK: - Enums
+    enum Duration {
+        case week, month, year
+    }
 
-    // MARK: - Variables
-
-    private let healthStore = HKHealthStore()
-
+    // MARK: - Properties
     private var weekStatistics  = StatisticsCollection()
     private var monthStatistics = StatisticsCollection()
     private var yearStatistics  = StatisticsCollection()
 
-    // MARK: - functions
+    var selectedDuration: Observable<Duration> = Observable(.week)
+    var selectedStatistics: Observable<(index: Int?, coordinate: Float?)> = Observable((nil, nil))
 
-    func statistics(for button: Button) -> StatisticsCollection {
-        switch button {
+    // MARK: - functions
+    func statistics() -> StatisticsCollection {
+        switch selectedDuration.value {
         case .week:
-            return self.weekStatistics
+            return weekStatistics
         case .month:
-            return self.monthStatistics
+            return monthStatistics
         case .year:
-            return self.yearStatistics
+            return yearStatistics
         }
     }
 
@@ -30,31 +33,68 @@ final class StatisticsViewModel {
         queryStepCountForYear()
     }
 
-    private func queryStepCountForWeek() {
-        queryStepCount(for: .week) { result in
-            self.weekStatistics = result
+    func tapStatistics(at xCoordinate: Float) {
+        let statisticsCollection: StatisticsCollection = statistics()
+        let offset = 1 / Float(statisticsCollection.count)
+        var selectedIndex: Int = -1
+
+        for index in 0..<statisticsCollection.count {
+            if Float(index) * offset <= xCoordinate && xCoordinate <= Float(index + 1) * offset {
+                selectedIndex = index
+                break
+            }
         }
-    }
-    private func queryStepCountForMonth() {
-        queryStepCount(for: .month) { result in
-            self.monthStatistics = result
-        }
-    }
-    private func queryStepCountForYear() {
-        queryStepCount(for: .year) { result in
-            self.yearStatistics = result
+
+        if selectedStatistics.value.index == selectedIndex {
+            selectedStatistics.value = (index: nil, coordinate: nil)
+        } else {
+            selectedStatistics.value = (index: selectedIndex, coordinate: Float(selectedIndex) * offset + offset / 2)
         }
     }
 
-    private func queryStepCount(for button: Button, completion: @escaping (StatisticsCollection) -> Void ) {
+    func panStatistics(at xCoordinate: Float) {
+        let statisticsCollection: StatisticsCollection = statistics()
+        let offset = 1 / Float(statisticsCollection.count)
+        var selectedIndex: Int = -1
+
+        for index in 0..<statisticsCollection.count {
+            if Float(index) * offset <= xCoordinate && xCoordinate <= Float(index + 1) * offset {
+                selectedIndex = index
+                break
+            }
+        }
+
+        if selectedStatistics.value.index != selectedIndex {
+            selectedStatistics.value = (index: selectedIndex, coordinate: Float(selectedIndex) * offset + offset / 2)
+        }
+    }
+
+    private func queryStepCountForWeek() {
+        queryStepCount(for: .week) { [weak self] result in
+            self?.weekStatistics = result
+        }
+    }
+    private func queryStepCountForMonth() {
+        queryStepCount(for: .month) { [weak self] result in
+            self?.monthStatistics = result
+        }
+    }
+    private func queryStepCountForYear() {
+        queryStepCount(for: .year) { [weak self] result in
+            self?.yearStatistics = result
+        }
+    }
+
+    private func queryStepCount(for button: Duration, completion: @escaping (StatisticsCollection) -> Void) {
         var interval: DateComponents
         var duration: Calendar.Component
+
         switch button {
         case .week:
             interval = DateComponents(day: 1)
             duration = .weekOfMonth
         case .month:
-            interval = DateComponents(day: 1)
+            interval = DateComponents(weekOfMonth: 1)
             duration = .month
         case .year:
             interval = DateComponents(month: 1)
@@ -62,17 +102,21 @@ final class StatisticsViewModel {
         }
 
         guard let stepCountType = HKQuantityType.quantityType(forIdentifier: .stepCount),
-              let oneyearago = Calendar.current.date(byAdding: duration, value: -1, to: Date()) else { return }
-        let startDate = Calendar.current.startOfDay(for: oneyearago)
-        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options: [.strictStartDate, .strictEndDate])
+              let past = Calendar.current.date(byAdding: duration,
+                                                     value: -1,
+                                                     to: Date())
+        else { return }
 
+        let startDate = Calendar.current.startOfDay(for: past)
         let anchorDate = Calendar.current.startOfDay(for: Date())
+        let predicate = HKQuery.predicateForSamples(withStart: startDate,
+                                                    end: Date(),
+                                                    options: [])
 
         HealthStoreManager.shared.requestStatisticsCollectionQuery(type: stepCountType,
                                                                    predicate: predicate,
                                                                    interval: interval,
                                                                    anchorDate: anchorDate) { hkStatisticsCollection in
-
             var statisticsCollection = StatisticsCollection()
 
             hkStatisticsCollection.enumerateStatistics(from: startDate, to: anchorDate) { (statistics, _) in
@@ -83,9 +127,8 @@ final class StatisticsViewModel {
                     statisticsCollection.append(statistics: statistics)
                 }
             }
-            completion(statisticsCollection)
 
+            completion(statisticsCollection)
         }
     }
-
 }
