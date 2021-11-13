ed88a7274fed5bcea561a21ec332fdc80b299358
KTH-INHA-16 <kim1120318@naver.com>
2021-11-13 11:30:07 +0900

    add: Repository Manager CURD close #75

--- Booster/Booster/Repositories/RepositoryManager.swift
+++ Booster/Booster/Repositories/RepositoryManager.swift
@@ -27,9 +27,8 @@ final class RepositoryManager {
             else { return }
 
             let entityObject = NSManagedObject(entity: entity, insertInto: self.container.viewContext)
-            value.forEach {
-                entityObject.setValue($0.value, forKey: $0.key)
-            }
+            value.forEach { entityObject.setValue($0.value, forKey: $0.key) }
+
             let context = self.container.viewContext
 
             do {
@@ -41,12 +40,32 @@ final class RepositoryManager {
         }
     }
 
+    func save<DataType: NSManagedObject>(value: [String: Any],
+              type name: String,
+              predicate: NSPredicate,
+              completion handler: @escaping (Result<DataType, Error>) -> Void) {
+        backgroundContext.perform { [weak self] in
+            guard let self = self
+            else { return }
+
+            let request = NSFetchRequest<DataType>.init(entityName: name)
+            request.predicate = predicate
+
+            do {
+                let objects = try self.container.viewContext.fetch(request)
+                value.forEach { objects[0].setValue($0.value, forKey: $0.key) }
+                try self.container.viewContext.save()
+                handler(.success(objects[0]))
+            } catch let error {
+                handler(.failure(error))
+            }
+        }
+    }
+
     func fetch<DataType: NSManagedObject>(completion handler: @escaping (Result<[DataType], Error>) -> Void) {
         backgroundContext.perform { [weak self] in
             guard let self = self
-            else {
-                return
-            }
+            else { return }
 
             do {
                 let context = try self.container.viewContext.fetch(DataType.fetchRequest())
@@ -54,6 +73,62 @@ final class RepositoryManager {
                 else { return }
 
                 handler(.success(context))
+            } catch let error {
+                handler(.failure(error))
+            }
+        }
+    }
+
+    func fetch<DataType: NSManagedObject>(entityName: String,
+                                          predicate: NSPredicate,
+                                          completion handler: @escaping (Result<[DataType], Error>) -> Void) {
+        backgroundContext.perform { [weak self] in
+            guard let self = self
+            else { return }
+
+            let request = NSFetchRequest<DataType>.init(entityName: entityName)
+            request.predicate = predicate
+            do {
+                let result = try self.container.viewContext.fetch(request)
+                handler(.success(result))
+            } catch let error {
+                handler(.failure(error))
+            }
+        }
+    }
+
+    func delete<DataType: NSManagedObject>(entityName: String,
+                                           predicate: NSPredicate,
+                                           completion handler: @escaping (Result<DataType, Error>) -> Void) {
+        backgroundContext.perform { [weak self] in
+            guard let self = self
+            else { return }
+
+            let request = NSFetchRequest<DataType>.init(entityName: entityName)
+            request.predicate = predicate
+
+            do {
+                let objects = try self.container.viewContext.fetch(request)
+                self.container.viewContext.delete(objects[0])
+                try self.container.viewContext.save()
+                handler(.success(objects[0]))
+            } catch let error {
+                handler(.failure(error))
+            }
+        }
+    }
+
+    func delete(entityName: String, completion handler: @escaping (Result<Void, Error>) -> Void) {
+        backgroundContext.perform { [weak self] in
+            guard let self = self
+            else { return }
+
+            let request = NSFetchRequest<NSFetchRequestResult>.init(entityName: entityName)
+
+            do {
+                let delete = NSBatchDeleteRequest(fetchRequest: request)
+                try self.container.viewContext.execute(delete)
+                handler(.success(()))
             } catch let error {
                 handler(.failure(error))
             }
