8826b07c20a1dec955f30f63cc9a2221a4550a0a
s1gnature <lamant0486@naver.com>
2021-11-16 17:31:36 +0900

    add: UserUsecase 추가

---
+++ Booster/Booster/Usecases/UserUsecase.swift
@@ -0,0 +1,24 @@
+//
+//  UserUsecase.swift
+//  Booster
+//
+//  Created by mong on 2021/11/16.
+//
+
+import Foundation
+
+final class UserUsecase {
+    private let repository = RepositoryManager()
+    
+    func removeAllUserData() {
+        
+    }
+    
+    func editUserInfo(userInfoModel: UserInfo) {
+        
+    }
+    
+    func changeGoal(to: Int) {
+        
+    }
+}
