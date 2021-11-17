902fa550758596780bd44079b7ef36d0512e4087
KTH-INHA-16 <kim1120318@naver.com>
2021-11-17 23:05:00 +0900

    add: 앱 시작시에 사용자 정보가 입력되었는지 판단하여 맞는 화면을 보여주는 것 구현 close #101

--- Booster/Booster/AppDelegates/SceneDelegate.swift
+++ Booster/Booster/AppDelegates/SceneDelegate.swift
@@ -4,6 +4,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        let isSaved = UserDefaults.standard.bool(forKey: "isUserInfoSaved")
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let enrollStoryboard = UIStoryboard(name: "Enroll", bundle: nil)
+        let mainViewController = mainStoryboard.instantiateViewController(withIdentifier: "MainNavigationViewController")
+        let enrollViewController = enrollStoryboard.instantiateViewController(withIdentifier: "EnrollNavigationViewController")
+
+        guard let scene = scene as? UIWindowScene
+        else { return }
+        window = UIWindow(windowScene: scene)
+
+        switch isSaved {
+        case true:
+            window?.rootViewController = mainViewController
+        case false:
+            window?.rootViewController = enrollViewController
+        }
     }
 }
