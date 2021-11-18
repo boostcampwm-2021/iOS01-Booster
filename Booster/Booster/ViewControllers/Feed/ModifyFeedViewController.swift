46fd8725d03101b4e3743f1ae6cfb03b224a52f7
Hiju <rose6649@likelion.org>
2021-11-18 15:02:20 +0900

    add: 글수정페이지뷰컨트롤러생성

--- Booster/Booster/ViewControllers/Feed/ModifyFeedViewController.swift
+++ Booster/Booster/ViewControllers/Feed/ModifyFeedViewController.swift
@@ -13,6 +13,7 @@ class ModifyFeedViewController: UIViewController, BaseViewControllerTemplate {
     // MARK: - @IBOutlet
 
     // MARK: - Properties
+    var viewModel = 0
 
     // MARK: - Subscript
 
@@ -21,11 +22,21 @@ class ModifyFeedViewController: UIViewController, BaseViewControllerTemplate {
     // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        configure()
     }
-    
+
     // MARK: - @IBActions
 
     // MARK: - @objc
+    @objc private func completedButtonDidTapped(_ sender: UIBarButtonItem) {
+    }
 
     // MARK: - Functions
+    func configure() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "완료",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(completedButtonDidTapped(_:)))
+    }
 }
