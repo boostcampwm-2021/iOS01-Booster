df26cc60b3167af1e9389c14aa14f3a7aef3bdfe
KTH-INHA-16 <kim1120318@naver.com>
2021-11-18 16:15:06 +0900

    refac: Scene Delegate 상수 처리

--- Booster/Booster/AppDelegates/SceneDelegate.swift
+++ Booster/Booster/AppDelegates/SceneDelegate.swift
@@ -1,14 +1,22 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    private enum KeyValue: String {
+        case isUserInfoSaved = "isUserInfoSaved"
+        case main = "Main"
+        case enroll = "enroll"
+        case mainNavigation = "MainNavigationViewController"
+        case enrollNavigation = "EnrollNavigationViewController"
+    }
+
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let isSaved = UserDefaults.standard.bool(forKey: "isUserInfoSaved")
-        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let enrollStoryboard = UIStoryboard(name: "Enroll", bundle: nil)
-        let mainViewController = mainStoryboard.instantiateViewController(withIdentifier: "MainNavigationViewController")
-        let enrollViewController = enrollStoryboard.instantiateViewController(withIdentifier: "EnrollNavigationViewController")
+        let isSaved = UserDefaults.standard.bool(forKey: KeyValue.isUserInfoSaved.rawValue)
+        let mainStoryboard = UIStoryboard(name: KeyValue.main.rawValue, bundle: nil)
+        let enrollStoryboard = UIStoryboard(name: KeyValue.enroll.rawValue, bundle: nil)
+        let mainViewController = mainStoryboard.instantiateViewController(withIdentifier: KeyValue.mainNavigation.rawValue)
+        let enrollViewController = enrollStoryboard.instantiateViewController(withIdentifier: KeyValue.enrollNavigation.rawValue)
 
         guard let scene = scene as? UIWindowScene
         else { return }
