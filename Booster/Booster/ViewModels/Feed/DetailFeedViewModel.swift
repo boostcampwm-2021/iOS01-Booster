efe4dc86039b91b9945f3aa9434b998e25b63216
KTH-INHA-16 <kim1120318@naver.com>
2021-11-20 11:17:27 +0900

    fix: detail feed view model initializer 변경 및 property 추가

--- Booster/Booster/ViewModels/Feed/DetailFeedViewModel.swift
+++ Booster/Booster/ViewModels/Feed/DetailFeedViewModel.swift
@@ -10,18 +10,21 @@ import Foundation
 final class DetailFeedViewModel {
     // MARK: - Properties
     private let usecase: DetailFeedUsecase
+    let startDate: Date
     var trackingModel: BoosterObservable<TrackingModel>
 
     // MARK: - Init
-    init() {
+    init(start date: Date) {
+        startDate = date
         trackingModel = BoosterObservable(TrackingModel())
         usecase = DetailFeedUsecase()
     }
 
     // MARK: - Functions
-    func fetchDetailFeedList(start date: Date) {
-        let predicate = NSPredicate(format: "startDate = %@", (date as NSDate) as CVarArg)
+    func fetchDetailFeedList() {
+        let predicate = NSPredicate(format: "startDate = %@", (startDate as NSDate) as CVarArg)
         usecase.fetch(predicate: predicate) { [weak self] result in
+            print("result", result)
             if let model = result.first {
                 self?.trackingModel.value = model
             }
