ffce1404458604744419b4c8f80e3408d0472f51
Hani <nellingprocess@sju.ac.kr>
2021-11-26 00:07:26 +0900

    refac: 홈 커스텀 라벨

--- Booster/Booster/ViewModels/Home/HomeViewModel.swift
+++ Booster/Booster/ViewModels/Home/HomeViewModel.swift
@@ -27,15 +27,15 @@ final class HomeViewModel {
 
     private func fetchTodayHourlyStepCountsData() {
         homeUsecase.fetchHourlyStepCountsData()
-            .subscribe { [weak self] hkStatisticsEvent in
+            .subscribe { [weak self] stepStatisticsCollection in
                 guard let self = self,
-                      let hkStatistics = hkStatisticsEvent.element,
-                      let entity = self.configureStepStatistics(using: hkStatistics)
+                      let stepStatisticsCollection = stepStatisticsCollection.element
                 else { return }
 
                 var newHomeModel = self.homeModel.value
-                newHomeModel.hourlyStatistics.append(entity)
+                newHomeModel.hourlyStatistics = stepStatisticsCollection
                 self.homeModel.accept(newHomeModel)
+
             }
             .disposed(by: disposeBag)
     }
@@ -47,12 +47,13 @@ final class HomeViewModel {
                       let statistics = result.element
                 else { return }
 
-                let distance = statistics.sumQuantity()?.doubleValue(for: HKUnit.meter()) ?? 0
+                let distance = statistics?.sumQuantity()?.doubleValue(for: HKUnit.meter()) ?? 0
                 let km = distance / 1000
 
                 var newHomeModel = self.homeModel.value
                 newHomeModel.km = km
                 self.homeModel.accept(newHomeModel)
+
             }
             .disposed(by: disposeBag)
     }
@@ -64,7 +65,7 @@ final class HomeViewModel {
                       let statistics = result.element
                 else { return }
 
-                let kcal = statistics.sumQuantity()?.doubleValue(for: HKUnit.kilocalorie()) ?? 0
+                let kcal = statistics?.sumQuantity()?.doubleValue(for: HKUnit.kilocalorie()) ?? 0
 
                 var newHomeModel = self.homeModel.value
                 newHomeModel.kcal = Int(kcal)
@@ -78,29 +79,19 @@ final class HomeViewModel {
         homeUsecase.fetchTodayTotalData(type: .stepCount)
             .subscribe { [weak self] result in
                 guard let self = self,
-                      let statistics = result.element,
-                      let seconds = statistics.duration()?.doubleValue(for: .second()),
-                      let sum = statistics.sumQuantity()?.doubleValue(for: .count())
+                      let statistics = result.element
                 else { return }
 
+                let time = statistics?.duration()?.doubleValue(for: .second()) ?? 0
+                let sum = statistics?.sumQuantity()?.doubleValue(for: .count()) ?? 0
+
                 var newHomeModel = self.homeModel.value
-                newHomeModel.activeTime = TimeInterval(seconds)
+                newHomeModel.activeTime = TimeInterval(time)
                 newHomeModel.totalStepCount = Int(sum)
+
                 self.homeModel.accept(newHomeModel)
 
             }
             .disposed(by: disposeBag)
-    }
-
-    private func configureStepStatistics(using statistics: HKStatistics) -> StepStatistics? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "ko_KR")
-        dateFormatter.dateFormat = "H"
-
-        let step = Int(statistics.sumQuantity()?.doubleValue(for: HKUnit.count()) ?? 0)
-        let date = statistics.startDate
-        let string = dateFormatter.string(from: date)
-
-        return StepStatistics(step: step, abbreviatedDateString: string)
     }
 }
