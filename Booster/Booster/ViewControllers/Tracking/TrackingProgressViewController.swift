630cac1bf0e977832b916b419c656ff63ab2dfab
KTH-INHA-16 <kim1120318@naver.com>
2021-11-27 22:00:04 +0900

    refac: 트래킹 info view 부분 리펙토링

--- Booster/Booster/ViewControllers/Tracking/TrackingProgressViewController.swift
+++ Booster/Booster/ViewControllers/Tracking/TrackingProgressViewController.swift
@@ -7,23 +7,8 @@ import RxCocoa
 final class TrackingProgressViewController: UIViewController, BaseViewControllerTemplate {
     // MARK: - @IBOutlet
     @IBOutlet weak var mapView: TrackingMapView!
-    @IBOutlet weak var pedometerLabel: UILabel!
-    @IBOutlet weak var kcalLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var distanceLabel: UILabel!
-    @IBOutlet weak var leftButton: UIButton!
-    @IBOutlet weak var rightButton: UIButton!
-    @IBOutlet weak var infoView: UIView!
-    @IBOutlet weak var pedometerTrailingConstraint: NSLayoutConstraint!
-    @IBOutlet weak var pedometerTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var infoView: TrackingInfoView!
     @IBOutlet weak var mapViewBottomConstraint: NSLayoutConstraint!
-    @IBOutlet weak var kcalTopConstraint: NSLayoutConstraint!
-    @IBOutlet weak var timeTopConstraint: NSLayoutConstraint!
-    @IBOutlet weak var distanceTopConstraint: NSLayoutConstraint!
-    @IBOutlet weak var rightButtonHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var rightButtonWidthConstraint: NSLayoutConstraint!
-    @IBOutlet weak var rightButtonTrailingConstraint: NSLayoutConstraint!
-    @IBOutlet weak var rightButtonBottomConstraint: NSLayoutConstraint!
 
     // MARK: - Properties
     private let pedometer = CMPedometer()
@@ -41,65 +26,12 @@ final class TrackingProgressViewController: UIViewController, BaseViewController
         pickerController.delegate = self
         return pickerController
     }()
-    private lazy var titleTextField: UITextField = {
-        let textField = UITextField(frame: self.view.frame)
-        let title = "제목"
-        textField.font = .notoSansKR(.medium, 25)
-        textField.textColor = .boosterLabel
-        textField.attributedPlaceholder = .makeAttributedString(text: title,
-                                                                font: .notoSansKR(.medium, 25),
-                                                                color: .lightGray)
-        textField.autocorrectionType = .no
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.rx.controlEvent([.editingDidEnd])
-            .bind {
-                self.rightButton.isHidden = false
-            }.disposed(by: disposeBag)
-        textField.rx.controlEvent([.editingDidBegin])
-            .bind {
-                self.rightButton.isHidden = true
-            }.disposed(by: disposeBag)
-        textField.rx.text
-            .distinctUntilChanged()
-            .skip(1)
-            .bind { [weak self] value in
-                guard let text = value
-                else { return }
-
-                self?.viewModel.title.onNext(text)
-            }.disposed(by: disposeBag)
-        textField.delegate = self
-        return textField
-    }()
-    private lazy var contentTextView: UITextView = {
-        let textView = UITextView()
-        let emptyText = "오늘 산책은 어땠나요?"
-        textView.backgroundColor = .clear
-        textView.font = .notoSansKR(.light, 17)
-        textView.text = emptyText
-        textView.textColor = .lightGray
-        textView.textContainer.lineFragmentPadding = 0
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.rx.text
-            .distinctUntilChanged()
-            .skip(1)
-            .bind { [weak self] value in
-                guard let text = value
-                else { return }
-
-                if textView.textColor != .lightGray {
-                    self?.viewModel.content.onNext(text)
-                }
-            }.disposed(by: disposeBag)
-        textView.delegate = self
-        return textView
-    }()
     private lazy var backButtonItem: UIBarButtonItem = {
         let buttonItem = UIBarButtonItem()
         buttonItem.image = .systemArrowLeft
         buttonItem.tintColor = .boosterBlackLabel
         buttonItem.rx.tap
-            .throttle(.milliseconds(800), scheduler: MainScheduler.asyncInstance)
+            .throttle(.seconds(1), scheduler: MainScheduler.asyncInstance)
             .bind { [weak self] in
                 let title = "되돌아가기"
                 let message = "현재 기록 상황이 다 지워집니다\n정말로 되돌아가실 건가요?"
@@ -135,7 +67,6 @@ final class TrackingProgressViewController: UIViewController, BaseViewController
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = false
         tabBarController?.tabBar.isHidden = true
-        manager.delegate = self
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -187,20 +118,9 @@ final class TrackingProgressViewController: UIViewController, BaseViewController
     }
 
     func configure() {
-        let radius: CGFloat = 50
         view.addSubview(userLocationButton)
-        leftButton.layer.borderWidth = 1
-        leftButton.layer.borderColor = UIColor.boosterBackground.cgColor
-        leftButton.layer.cornerRadius = radius
-        rightButton.layer.cornerRadius = radius
-        pedometerLabel.font = .bazaronite(size: 60)
-        pedometerLabel.textColor = .boosterBlackLabel
         userLocationButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         userLocationButton.bottomAnchor.constraint(equalTo: infoView.topAnchor, constant: -20).isActive = true
-
-        [mapView, kcalLabel, timeLabel, distanceLabel, pedometerLabel, rightButton].forEach {
-            $0?.translatesAutoresizingMaskIntoConstraints = false
-        }
 
         navigationItem.hidesBackButton = true
         navigationItem.leftBarButtonItem = backButtonItem
@@ -252,7 +172,36 @@ final class TrackingProgressViewController: UIViewController, BaseViewController
     }
 
     private func bindView() {
-        leftButton.rx.tap
+        infoView.titleTextField.rx.controlEvent([.editingDidEnd])
+            .bind {
+                self.infoView.rightButton.isHidden = false
+            }.disposed(by: disposeBag)
+        infoView.titleTextField.rx.controlEvent([.editingDidBegin])
+            .bind {
+                self.infoView.rightButton.isHidden = true
+            }.disposed(by: disposeBag)
+        infoView.titleTextField.rx.text
+            .distinctUntilChanged()
+            .skip(1)
+            .bind { [weak self] value in
+                guard let text = value
+                else { return }
+
+                self?.viewModel.title.onNext(text)
+            }.disposed(by: disposeBag)
+        infoView.contentTextView.rx.text
+            .distinctUntilChanged()
+            .skip(1)
+            .bind { [weak self] value in
+                guard let text = value,
+                      let textColor = self?.infoView.contentTextView.textColor
+                else { return }
+
+                if textColor != .lightGray {
+                    self?.viewModel.content.onNext(text)
+                }
+            }.disposed(by: disposeBag)
+        infoView.leftButton.rx.tap
             .throttle(.microseconds(1500), scheduler: MainScheduler.asyncInstance)
             .bind { [weak self] _ in
                 guard let self = self
@@ -294,7 +243,7 @@ final class TrackingProgressViewController: UIViewController, BaseViewController
                 }
             }.disposed(by: disposeBag)
 
-        rightButton.rx.tap
+        infoView.rightButton.rx.tap
             .throttle(.seconds(1), scheduler: MainScheduler.asyncInstance)
             .bind { [weak self] _ in
                 guard let self = self
@@ -337,7 +286,9 @@ final class TrackingProgressViewController: UIViewController, BaseViewController
         viewModel.trackingModel
             .asDriver()
             .drive(onNext: { [weak self] trackingModel in
-                self?.configure(model: trackingModel)
+                guard let state = self?.viewModel.state.value
+                else { return }
+                self?.infoView.configure(model: trackingModel, state: state)
             }).disposed(by: disposeBag)
 
         viewModel.cachedMilestones
@@ -361,42 +312,9 @@ final class TrackingProgressViewController: UIViewController, BaseViewController
             }).disposed(by: disposeBag)
     }
 
-    private func configure(model: TrackingModel) {
-        let timeContent = makeTimerText(time: model.seconds)
-        let kcalContent = "\(model.calories)\n"
-        let distanceContent = "\(String.init(format: "%.1f", model.distance/1000))\n"
-        let stepsTitle = "\(viewModel.state.value == .end ? " steps" : "")"
-        let kcalTitle = "kcal"
-        let timeTitle = "time"
-        let distanceTitle = "km"
-        let stepsColor: UIColor = viewModel.state.value == .end ? .boosterOrange : .boosterBlackLabel
-        let color: UIColor = viewModel.state.value == .start ? .boosterBackground : .boosterLabel
-
-        pedometerLabel.attributedText = makeAttributedText(content: "\(model.steps)",
-                                                           title: stepsTitle,
-                                                           contentFont: .bazaronite(size: 60),
-                                                           titleFont: .notoSansKR(.regular, 20),
-                                                           color: stepsColor)
-        kcalLabel.attributedText = makeAttributedText(content: kcalContent, title: kcalTitle, color: color)
-        timeLabel.attributedText = makeAttributedText(content: timeContent, title: timeTitle, color: color)
-        distanceLabel.attributedText = makeAttributedText(content: distanceContent, title: distanceTitle, color: color)
-    }
-
     private func update() {
         let isStart: Bool = viewModel.state.value == .start
-        [distanceLabel, timeLabel, kcalLabel].forEach {
-            $0?.textColor = isStart ? .boosterBackground : .boosterLabel
-        }
-
-        infoView.backgroundColor = isStart ? .boosterOrange : .boosterBackground
-        rightButton.backgroundColor = isStart ? .boosterBackground : .boosterOrange
-        leftButton.backgroundColor = isStart ? .boosterOrange : .boosterBackground
-        leftButton.layer.borderColor = isStart ? UIColor.boosterBackground.cgColor : UIColor.boosterOrange.cgColor
-        leftButton.tintColor = isStart ? .boosterBackground : .boosterOrange
-        rightButton.tintColor = isStart ? .boosterOrange : .boosterBackground
-        rightButton.setImage(isStart ? .systemPause : .systemPlay, for: .normal)
-        leftButton.setImage(isStart ? .systemCamera : .systemStop, for: .normal)
-        timerDate = isStart ? Date() : timerDate
+        infoView.update(state: viewModel.state.value)
 
         switch isStart {
         case true:
@@ -415,94 +333,22 @@ final class TrackingProgressViewController: UIViewController, BaseViewController
         }
     }
 
-    private func configureWrite() {
-        infoView.addSubview(titleTextField)
-        infoView.addSubview(contentTextView)
-        titleTextField.topAnchor.constraint(equalTo: kcalLabel.bottomAnchor, constant: 40).isActive = true
-        titleTextField.trailingAnchor.constraint(equalTo: infoView.trailingAnchor, constant: -25).isActive = true
-        titleTextField.leadingAnchor.constraint(equalTo: infoView.leadingAnchor, constant: 25).isActive = true
-        contentTextView.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 10).isActive = true
-        contentTextView.bottomAnchor.constraint(equalTo: infoView.bottomAnchor, constant: -10).isActive = true
-        contentTextView.trailingAnchor.constraint(equalTo: infoView.trailingAnchor, constant: -25).isActive = true
-        contentTextView.leadingAnchor.constraint(equalTo: infoView.leadingAnchor, constant: 25).isActive = true
-    }
-
     private func stopAnimation() {
-        let title = " steps"
-        let content = "\(viewModel.trackingModel.value.steps)"
-
         stopTracking()
-
-        leftButton.isHidden = true
-        userLocationButton.isHidden = true
-        pedometerLabel.attributedText = makeAttributedText(content: content,
-                                                                     title: title,
-                                                                     contentFont: .bazaronite(size: 60),
-                                                                     titleFont: .notoSansKR(.regular, 20),
-                                                                     color: .boosterOrange)
-        pedometerLabel.sizeToFit()
+        infoView.stopPedometerText()
 
         UIView.animate(withDuration: 1, animations: { [weak self] in
             guard let self = self
             else { return }
-
-            self.rightButtonWidthConstraint.constant = 70
-            self.rightButtonHeightConstraint.constant = 70
-            self.rightButton.layer.cornerRadius = 35
-            self.rightButtonTrailingConstraint.constant = 25
-            self.rightButtonBottomConstraint.constant = 25
+            self.infoView.stopAnimation()
             self.mapViewBottomConstraint.constant = self.view.frame.maxY - 290
-            self.pedometerTrailingConstraint.constant = self.view.frame.maxX - self.pedometerLabel.frame.width - 25
-            self.pedometerTopConstraint.constant = 20
-            [self.timeTopConstraint, self.kcalTopConstraint, self.distanceTopConstraint].forEach {
-                $0.constant = 130
-            }
-            self.rightButton.setImage(.systemPencil, for: .normal)
-            self.pedometerLabel.attributedText = self.makeAttributedText(content: content,
-                                                                         title: title,
-                                                                         contentFont: .bazaronite(size: 60),
-                                                                         titleFont: .notoSansKR(.regular, 20),
-                                                                         color: .boosterOrange)
             self.view.layoutIfNeeded()
-            self.infoView.layoutIfNeeded()
         }, completion: { [weak self] _ in
             guard let self = self
             else { return }
 
-            self.configureWrite()
-            self.infoView.bringSubviewToFront(self.rightButton)
+            self.infoView.configureWrite()
         })
-    }
-
-    private func makeTimerText(time: Int) -> String {
-        let seconds = time % 60
-        let minutes = time / 60
-        var text = ""
-        text += "\(minutes < 10 ? "0\(minutes)'" : "\(minutes)'")"
-        text += "\(seconds < 10 ? "0\(seconds)\"\n" : "\(seconds)\"\n")"
-        return text
-    }
-
-    private func makeAttributedText(content: String,
-                                    title: String,
-                                    contentFont: UIFont = .bazaronite(size: 30),
-                                    titleFont: UIFont = .notoSansKR(.light, 15),
-                                    color: UIColor = .black)
-    -> NSMutableAttributedString {
-        let mutableString = NSMutableAttributedString()
-
-        let contentText: NSAttributedString = .makeAttributedString(text: content,
-                                                                    font: contentFont,
-                                                                    color: color)
-        let titleText: NSAttributedString = .makeAttributedString(text: title,
-                                                                  font: titleFont,
-                                                                  color: color)
-
-        [contentText, titleText].forEach {
-            mutableString.append($0)
-        }
-
-        return mutableString
     }
 
     private func makeImageData() {
@@ -671,43 +517,6 @@ extension TrackingProgressViewController: UIImagePickerControllerDelegate & UINa
             viewModel.append(of: milestone)
         }
         picker.dismiss(animated: true, completion: nil)
-    }
-}
-
-// MARK: text view delegate
-extension TrackingProgressViewController: UITextViewDelegate {
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        if textView.textColor == UIColor.lightGray {
-            textView.text = nil
-            textView.textColor = .boosterLabel
-        }
-        rightButton.isHidden = true
-    }
-
-    func textViewDidEndEditing(_ textView: UITextView) {
-        if textView.text.isEmpty {
-            let emptyText = "오늘 산책은 어땠나요?"
-            textView.text = emptyText
-            textView.textColor = .lightGray
-        }
-        rightButton.isHidden = false
-    }
-}
-
-// MARK: text field delegate
-extension TrackingProgressViewController: UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        guard let text = textField.text
-        else { return true }
-
-        let maximum = 15
-
-        return text.count + string.count < maximum
     }
 }
 
