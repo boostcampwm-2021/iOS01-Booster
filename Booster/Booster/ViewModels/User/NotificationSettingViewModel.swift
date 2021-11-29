6474e10a2e9d165807c39961c13faac9a1b2553e
s1gnature <lamant0486@naver.com>
2021-11-29 15:03:21 +0900

    refactor: NotificationSettingViewModel state를 bind로 리팩토링

--- Booster/Booster/ViewModels/User/NotificationSettingViewModel.swift
+++ Booster/Booster/ViewModels/User/NotificationSettingViewModel.swift
@@ -26,36 +26,46 @@ final class NotificationSettingViewModel {
     }
 
     private(set) var model = BehaviorRelay<NotificationSettingModel>(value: NotificationSettingModel())
-    private var notificationState: NotificationState?
+    private let disposeBag = DisposeBag()
+    private var notificationState = PublishRelay<NotificationState?>()
 
-    init() {}
+    init() {
+        bind()
+    }
+    
     init(state: NotificationState) {
-        notificationState = state
-        setState(to: state)
+        bind()
+        notificationState.accept(state)
     }
 
     func setState(to state: NotificationState) {
-        (notificationState == state) ? () : changeModelValue(state: state)
+        notificationState.accept(state)
     }
-
-    private func changeModelValue(state: NotificationState) {
-        var newModel = model.value
-        switch state {
-        case .on:
-            newModel.title = "현재 알림이 켜져있어요"
-            newModel.subTitle = "좋은 소식들을\n들려드리기 위해 열심히 노력하고 있어요!"
-            newModel.image = UIImage.notificationOn
-            newModel.buttonBackgroundColor = .boosterEnableButtonGray
-            newModel.buttonAttributedTitle = NSAttributedString(string: "알림 끄기")
-            newModel.buttonTintColor = .boosterGray
-        case .off:
-            newModel.title = "현재 알림이 꺼져있어요"
-            newModel.subTitle = "알람을 키면\n좋은 소식들을 가득 들려드릴게요"
-            newModel.image = UIImage.notificationOff
-            newModel.buttonBackgroundColor = .boosterOrange
-            newModel.buttonAttributedTitle = NSAttributedString(string: "알림 켜기")
-            newModel.buttonTintColor = .boosterBlackLabel
-        }
-        model.accept(newModel)
+    
+    private func bind() {
+        notificationState
+            .bind(onNext: { [weak self] state in
+                guard let self = self
+                else { return }
+                
+                var newModel = self.model.value
+                switch state {
+                case .on:
+                    newModel.title = "현재 알림이 켜져있어요"
+                    newModel.subTitle = "좋은 소식들을\n들려드리기 위해 열심히 노력하고 있어요!"
+                    newModel.image = UIImage.notificationOn
+                    newModel.buttonBackgroundColor = .boosterEnableButtonGray
+                    newModel.buttonAttributedTitle = NSAttributedString(string: "알림 끄기")
+                    newModel.buttonTintColor = .boosterGray
+                case .off, .none:
+                    newModel.title = "현재 알림이 꺼져있어요"
+                    newModel.subTitle = "알람을 키면\n좋은 소식들을 가득 들려드릴게요"
+                    newModel.image = UIImage.notificationOff
+                    newModel.buttonBackgroundColor = .boosterOrange
+                    newModel.buttonAttributedTitle = NSAttributedString(string: "알림 켜기")
+                    newModel.buttonTintColor = .boosterBlackLabel
+                }
+                self.model.accept(newModel)
+            }).disposed(by: disposeBag)
     }
 }
