9f03d0d0b6eaebc2b677e78c88865d09f6979716
s1gnature <lamant0486@naver.com>
2021-11-30 01:26:33 +0900

    fix: NotificationSettingViewModel에서 UIKit 요소 제거

--- Booster/Booster/ViewModels/User/NotificationSettingViewModel.swift
+++ Booster/Booster/ViewModels/User/NotificationSettingViewModel.swift
@@ -8,7 +8,6 @@
 import Foundation
 import RxSwift
 import RxRelay
-import UIKit
 
 final class NotificationSettingViewModel {
     enum NotificationState {
@@ -17,12 +16,12 @@ final class NotificationSettingViewModel {
     }
 
     struct NotificationSettingModel {
-        var title: String = ""
-        var subTitle: String = ""
-        var image: UIImage = UIImage()
-        var buttonBackgroundColor: UIColor = .systemBackground
+        var title = ""
+        var subTitle = ""
+        var imageName = ""
+        var buttonBackgroundColorName = ""
         var buttonAttributedTitle: NSAttributedString = NSAttributedString()
-        var buttonTintColor: UIColor = .boosterLabel
+        var buttonTintColorName = ""
     }
 
     private(set) var model = BehaviorRelay<NotificationSettingModel>(value: NotificationSettingModel())
@@ -53,17 +52,17 @@ final class NotificationSettingViewModel {
                 case .on:
                     newModel.title = "현재 알림이 켜져있어요"
                     newModel.subTitle = "좋은 소식들을\n들려드리기 위해 열심히 노력하고 있어요!"
-                    newModel.image = UIImage.notificationOn
-                    newModel.buttonBackgroundColor = .boosterEnableButtonGray
+                    newModel.imageName = "notificationOn"
+                    newModel.buttonBackgroundColorName = "boosterEnableButtonGray"
                     newModel.buttonAttributedTitle = NSAttributedString(string: "알림 끄기")
-                    newModel.buttonTintColor = .boosterGray
+                    newModel.buttonTintColorName = "boosterGray"
                 case .off, .none:
                     newModel.title = "현재 알림이 꺼져있어요"
                     newModel.subTitle = "알람을 키면\n좋은 소식들을 가득 들려드릴게요"
-                    newModel.image = UIImage.notificationOff
-                    newModel.buttonBackgroundColor = .boosterOrange
+                    newModel.imageName = "notificationOff"
+                    newModel.buttonBackgroundColorName = "boosterOrange"
                     newModel.buttonAttributedTitle = NSAttributedString(string: "알림 켜기")
-                    newModel.buttonTintColor = .boosterBlackLabel
+                    newModel.buttonTintColorName = "boosterBlackLabel"
                 }
                 self.model.accept(newModel)
             }).disposed(by: disposeBag)
